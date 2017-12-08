653fcb02b6354c1f54f4f8e03a76dd51edddb4ad
grisha-chasovskih <chasovskihgrisha@gmail.com>
2017-12-08 21:52:03 +0100

    Implement PoloniexClient

---
+++ src/PoloniexClient.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * This file is part of Poloniex PHP SDK.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright 2017-2018 Chasovskih Grisha <chasovskihgrisha@gmail.com>
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ */
+
+namespace Poloniex;
+
+use GuzzleHttp\Client;
+use Poloniex\CallHistory\CallHistoryInterface;
+use Psr\Log\{LoggerAwareTrait, LoggerInterface};
+
+/**
+ * Class PoloniexClient
+ *
+ * NOTE: Currency pairs are reverse of what most exchanges use.
+ *       For instance, instead of XPM_BTC, use BTC_XPM
+ *
+ * @link https://poloniex.com/support/api/
+ */
+class PoloniexClient extends Client
+{
+    use LoggerAwareTrait;
+
+    /**
+     * Base url for poloniex api requests
+     */
+    public const BASE_URI = 'https://poloniex.com/';
+
+    /**
+     * @var CallHistoryInterface
+     */
+    private $callHistory;
+
+    /**
+     * PoloniexClient constructor.
+     *
+     * @param LoggerInterface      $logger
+     * @param CallHistoryInterface $callHistory
+     * @param string               $timeout
+     * @param string               $baseUri
+     */
+    public function __construct(
+        LoggerInterface $logger,
+        CallHistoryInterface $callHistory,
+        string $timeout,
+        string $baseUri = self::BASE_URI
+    ) {
+        $this->setLogger($logger);
+        $this->callHistory = $callHistory;
+
+        parent::__construct([
+            'base_uri'        => $baseUri,
+            'timeout'         => $timeout,
+            'allow_redirects' => false,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function request($method, $uri = '', array $options = [])
+    {
+        if ($this->callHistory->isIncreased()) {
+            $this->logger->warning('Limit increased. Sleep for 1 second.');
+            sleep(1);
+        }
+
+        $method = strtoupper($method);
+        $this->logger->info(sprintf('Send %s request to %s', $method, $this->getConfig('base_uri') . $uri), $options);
+
+        return parent::request(strtoupper($method), $uri, $options);
+    }
+
+    /**
+     * Check whether service is accessible
+     *
+     * @return bool
+     */
+    public function isInaccessible(): bool
+    {
+        return $this->get('/')->getStatusCode() !== 200;
+    }
+}

658b556ee10b2a103f7eef8d75f28c41b3441304
grisha-chasovskih <chasovskihgrisha@gmail.com>
2017-12-08 19:12:01 +0100

    Fix mistake

--- src/Exception/PoloniexException.php
+++ src/Exception/PoloniexException.php
@@ -5,7 +5,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @copyright 2017-2018 Chasovskih Grisha <chasovskihgrisha@gamil.com>
+ * @copyright 2017-2018 Chasovskih Grisha <chasovskihgrisha@gmail.com>
  * @license   http://www.opensource.org/licenses/mit-license.php MIT
  */
 
